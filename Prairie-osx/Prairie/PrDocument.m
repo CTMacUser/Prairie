ea46ad65a3f46d1a3952535242ad176101570313
Daryle Walker <dwalker07@yahoo.com>
2014-07-28 14:48:40 -0400

    Add first attempt at local load & save support.
    Set property list to indicate that the app can read any non-directory
    file, write the same or web-archives, and acts as a non-originator of
    those formats.
    
    Add a file-export menu item.
    
    Add private properties to the Document class to save a file’s
    attributes on document-load so they can be used when the web-view is
    later loaded from the document’s XIB. Use the web-view’s data as the
    source during saving. Disable auto-saving. Touch-up XIB loading.

--- Prairie-osx/Prairie/PrDocument.m
+++ Prairie-osx/Prairie/PrDocument.m
@@ -8,7 +8,25 @@
 
 #import "PrDocument.h"
 
+#pragma mark Private members
+
+@interface PrDocument ()
+
+@property (nonatomic, copy) NSURL *    initialURL;
+@property (nonatomic, copy) NSData *   initialData;
+@property (nonatomic, copy) NSString * initialType;  // MIME type, not UTI
+
+@end
+
 @implementation PrDocument
+
+#pragma mark Private data
+
+@synthesize initialURL;
+@synthesize initialData;
+@synthesize initialType;
+
+#pragma mark Conventional overrides
 
 - (id)init
 {
@@ -21,40 +39,66 @@
 
 - (NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-    return @"PrDocument";
+    return NSStringFromClass([self class]);
 }
 
 - (void)windowControllerDidLoadNib:(NSWindowController *)aController
 {
     [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
-    [[self.webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
+
+    // Load the first page.
+    if ( self.initialURL && self.initialData ) {
+        // Opening a file, with pre-loaded information (from the "readFrom..." methods).
+        [[self.webView mainFrame] loadData:self.initialData MIMEType:self.initialType textEncodingName:nil baseURL:self.initialURL];  // Can/should the initial* properties be cleared after this point?
+    } else {
+        // Open home page. (TODO: have this be a preference; support blank page.)
+        [[self.webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
+    }
 }
 
 + (BOOL)autosavesInPlace
 {
-    return YES;
+    return NO;  // viewer-only app
 }
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    WebDataSource * const  source = self.webView.mainFrame.dataSource;
+    NSData * const         data = [typeName isEqualToString:(__bridge NSString *)kUTTypeWebArchive] ? source.webArchive.data : source.data;  // Assumes typeName is the actual UTI for current web object when the object isn't a web-archive.
+
+    if ( !data && outError ) {
+        *outError = [[NSError alloc] initWithDomain:NSCocoaErrorDomain code:NSFileWriteUnknownError userInfo:nil];  // Is the error code appropriate?
+    }
+    return data;
+    // TODO: currently doesn't work right. Regular saves (and save-as) and reverts seem to be bad ideas. Export seems to work when web-archive is the format, but not public-content. The latter has no extension/type-information to pass on, so the exported file is untyped and useless. Is my assumption on the line for local variable "data" wrong? Should the app switch from Editor to Viewer? (That will require making a subclass of NSDocumentController to open windows correctly.)
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    self.initialData = data;
+    return YES;  // misnomer: data isn't actually read until the XIB's WebView loads it; can't get errors either
+}
+
+- (BOOL)readFromURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError *__autoreleasing *)outError
+{
+    // The default implementation calls the NSFileWrapper version, which calls the NSData version.  The WebView takes in an NSData and NSURL, so we need two overrides to get all the information.  The URL data will always be grabbed as long as calling code only uses the URL version and never skips ahead to directly call the NSData version.
+    CFStringRef const  mimeType = UTTypeCopyPreferredTagWithClass((__bridge CFStringRef) typeName, kUTTagClassMIMEType);
+
+    self.initialType = mimeType ? (__bridge NSString *)mimeType : @"application/octet-stream";
+    if ( [WebView canShowMIMEType:self.initialType] ) {
+        self.initialURL = url;
+        return [super readFromURL:url ofType:typeName error:outError];
+    }
+    if ( outError ) {
+        *outError = [[NSError alloc] initWithDomain:NSCocoaErrorDomain code:NSFileReadCorruptFileError userInfo:@{NSURLErrorKey: url}];  // Is the error code appropriate?
+    }
+    return NO;  // Cannot proceed if WebView can't process the required MIME type.
+}
+
+- (BOOL)isEntireFileLoaded
+{
+    // The file is loaded when the WebView finishes with it.
+    return self.webView && !self.webView.isLoading;
 }
 
 @end
